dd152c209d5104fa7f5bed35b2dc622abd8c632f
Andrew VanOsten <avanosten@wpi.edu>
2019-12-07 15:34:06 -0500

    Lots more incremental updates - added state machine, takeoff, general stuff

--- src/flight_code/flight_code.ino
+++ src/flight_code/flight_code.ino
@@ -21,23 +21,92 @@
 #define DEBUG_BAUD 115200
 #define MAV_BAUD   19200
 
+// System limitations
+#define MAX_MINES 128   // Max number of mines in minefield
+#define MINES_PER_RUN 6 // Max number of payloads that can be carried
+
+// Flight characteristics
+// Altitudes are relative to the takeoff point (home position)
+#define OPERATING_ALT 20  // The altitude at which the drone will move from point to point
+#define DROP_ALT      20  // The altitude at which the drone will drop payloads
+
+// Command status variable
+// Each time we send a command to the Pixhawk, we need to wait for the appropriate response from the Pixhawk
+// These variables track the response so we can reference it in the state machine
+enum command_status_t {ACCEPTED, IN_PROGRESS, COMPLETED} command_status;
+
+// State machine states
+// I'm sure we'll need to add more of these
+enum state_t {DISARMED, ARMED, TAKEOFF, HOVER, APPROACH, READY_TO_DROP, ESCAPE} state;
+
+// Mine datatype
+struct mine_t {
+  int32_t lat;
+  int32_t lon;
+  bool isDetonated;
+};
+
+uint8_t num_mines;              // The total number of mines in the minefield
+uint8_t minefield_index = 0;    // Current position in the minefield array
+mine_t minesfield[MAX_MINES];   // Array of mines that makes up the minefield
+
 void setup() {
   DEBUG_PORT.begin(DEBUG_BAUD);
   MAV_PORT.begin(MAV_BAUD);
+
+  // Read ordered minefield data (flight path) from base station, into minefield array
+  // TODO: ^
+  // Program should not pass this point until it has minefield data
 }
 
 void loop() {
-  // STATE MACHINE WILL GO HERE
-  // TODO: state machine
+  // Main state machine - this hsndles the primary program flow
+  switch(state) {
+    case DISARMED:      // Disarmed, sitting on the ground
+      // If we get the go signal from the base station:
+      //   arm();
+      //   state = ARMED;
+      // Not 100% sure it's possible to arm without controller input - needs more testing
+      break;
+    case ARMED:         // Armed, ready to takeoff
+      // Just chilling? Might not need this one
+      break;
+    case TAKEOFF:       // Actively taking off
+      takeoff();
+      // listen for ACK response, and wait until complete
+      if(command_status == ACCEPTED) {
+        command_status = COMPLETED; // Mark it as taken care of
+        // takeoff complete, now we can move on.
+        // TODO: [update state here]
+      }
+      break;
+    case HOVER:         // In the air, hovering idly
 
-  // At the end of each loop:
+      break;
+    case APPROACH:      // Approaching a mine
+
+      break;
+    case READY_TO_DROP: // Drone is over the mine, time to drop
+
+      break;
+    case ESCAPE:        // Drone just dropped a payload, should now be running away
+
+      break;
+  }
+  
   // Check for incoming messages from the base station
   // TODO: [Insert function call here]
   
   // Check for incoming messages from the Pixhawk
   mavlink_message_t msg_in;
   mavlink_status_t stat_in;
-  receive_mavlink(&msg_in, &stat_in);
+  if(receive_mavlink(&msg_in, &stat_in)) {
+    switch(msg_in.msgid) {
+      case MAVLINK_MSG_ID_COMMAND_ACK:
+        set_globals(&msg_in, &stat_in);
+        break;
+    }
+  }
 }
 
 // Reads from MAV_PORT until either:
@@ -68,8 +137,26 @@ void send_mavlink(mavlink_message_t msg) {
   MAV_PORT.write(buf,len);  // Send the message
 }
 
+// Takes a received message and updates global status variables
+void set_globals(mavlink_message_t *msg, mavlink_status_t *stat) {
+  uint8_t result = mavlink_msg_command_ack_get_result(msg);
+  if(result == MAV_RESULT_ACCEPTED) {
+      command_status = ACCEPTED;
+  }
+  else if(result == MAV_RESULT_TEMPORARILY_REJECTED ||
+          result == MAV_RESULT_DENIED ||
+          result == MAV_RESULT_UNSUPPORTED ||
+          result == MAV_RESULT_FAILED) {
+    // The Pixhawk returned an error - send something back to the base station
+    // TODO: Send and error message to the base station
+  }
+  else if(result == MAV_RESULT_IN_PROGRESS) {
+    command_status = IN_PROGRESS;
+  }
+}
+
 // Arms the drone
-void arm_drone() {
+void arm() {
   mavlink_message_t msg;
   // Pack a MAV_CMD_COMPONENT_ARM_DISARM command with the arm parameter set,
   // into a command_long message
@@ -78,10 +165,21 @@ void arm_drone() {
 }
 
 // Disarm the drone
-void disarm_drone() {
+void disarm() {
   mavlink_message_t msg;
   // Pack a MAV_CMD_COMPONENT_ARM_DISARM command with the arm parameter cleared,
   // into a command_long message
   mavlink_msg_command_long_pack(SYS_ID, COMP_ID, &msg, TARGET_SYS, TARGET_COMP, MAV_CMD_COMPONENT_ARM_DISARM, 0, 0,0,0,0,0,0,0); // DISARM
+  send_mavlink(msg);
+}
+
+// Takeoff
+// Takes off and climbs to OPERATING_ALT
+// Maintains lat/lon position
+void takeoff() {
+  mavlink_message_t msg;
+  // Pack a MAV_CMD_NAV_TAKEOFF command with the altitude parameter set,
+  // into a command_long message
+  mavlink_msg_command_long_pack(SYS_ID, COMP_ID, &msg, TARGET_SYS, TARGET_COMP, MAV_CMD_NAV_TAKEOFF, 0, 0,0,0,0,0,0,OPERATING_ALT); // TAKEOFF
   send_mavlink(msg);
 }
